862a8c14c90007127c33d9a960b2239a7c4bbf47
Ny Nguyen <85854283+nyxinhtrai@users.noreply.github.com>
2022-12-07 08:57:00 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,84 +1,81 @@
-const socket = io('https://audiocallgroup7.onrender.com', {transports: ['websocket', 'polling', 'flashsocket']});
+const socket = io("https://p2p-audio-call-server-production.up.railway.app/");
 
-$('#div-chat').hide();
+$("#div-chat").hide();
 
+socket.on("DANH_SACH_ONLINE", (arrUserInfo) => {
+	$("#div-chat").show();
+	$("#div-dang-ky").hide();
 
-socket.on('DANH_SACH_ONLINE', arrUserInfo => {
-    $('#div-chat').show();
-    $('#div-dang-ky').hide();
+	arrUserInfo.forEach((user) => {
+		const { ten, peerId } = user;
+		$("#ulUser").append(`<li id="${peerId}">${ten}</li>`);
+	});
 
-    arrUserInfo.forEach(user => {
-        const { ten, peerId } = user;
-        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
-    });
-    
-    socket.on('CO_NGUOI_DUNG_MOI', user => {
-        const { ten, peerId } = user;
-        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
-    });
+	socket.on("CO_NGUOI_DUNG_MOI", (user) => {
+		const { ten, peerId } = user;
+		$("#ulUser").append(`<li id="${peerId}">${ten}</li>`);
+	});
 
-    socket.on('AI_DO_NGAT_KET_NOI', peerId => {
-        $(`#${peerId}`).remove();
-    });
+	socket.on("AI_DO_NGAT_KET_NOI", (peerId) => {
+		$(`#${peerId}`).remove();
+	});
 });
 
-socket.on('DANG_KY_THAT_BAT', () => alert('Vui long chon username khac!'));
-
+socket.on("DANG_KY_THAT_BAI", () => alert("Vui long chon username khac!"));
 
 function openStream() {
-    const config = { audio: true, video: false };
-    return navigator.mediaDevices.getUserMedia(config);
+	const config = { audio: true, video: false };
+	return navigator.mediaDevices.getUserMedia(config);
 }
 
 function playStream(idAudioTag, stream) {
-    const audio = document.getElementById(idAudioTag);
-    audio.srcObject = stream;
-    audio.play();
+	const audio = document.getElementById(idAudioTag);
+	audio.srcObject = stream;
+	audio.play();
 }
 
-const peer = new Peer({ 
-    key: 'peerjs',
-    host: 'https://audiocallgroup7.onrender.com',
-    secure: true,
-    port: 443
+const peer = new Peer();
+
+peer.on("open", (id) => {
+	$("#my-peer").append(id);
+	$("#btnSignUp").on("click", () => {
+		const username = $("#txtUsername").val();
+		socket.emit("NGUOI_DUNG_DANG_KY", { ten: username, peerId: id });
+	});
 });
 
-peer.on('open', id => {
-    $('#my-peer').append(id)
-    $('#btnSignUp').on("click", () => {
-        const username = $('#txtUsername').val();
-        socket.emit('NGUOI_DUNG_DANG_KY', { ten: username, peerId: id });
-    });
+// Make Call By Clicking Online User's Name
+$("#ulUser").on("click", "li", function () {
+	const id = $(this).attr("id");
+	const peerId = $("#my-peer").text();
+	if (id === peerId) return alert("Khong the thuc hien cuoc goi cho ban than");
+	openStream().then((stream) => {
+		const call = peer.call(id, stream);
+		call.on("stream", (remoteStream) =>
+			playStream("remoteStream", remoteStream)
+		);
+	});
 });
 
-// Thuc hien cuoc goi bang click ten
-$('#ulUser').on('click', 'li', function() {
-    const id = $(this).attr('id');
-    openStream()
-    .then(stream => {
-        //playStream('localStream', stream);
-        const call = peer.call(id, stream);
-        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
-    });
-});
-
-// Caller
-$('#btnCall').on("click",() => {
-    const id = $('#remoteId').val();
-    openStream()
-    .then(stream => {
-        //playStream('localStream', stream);
-        const call = peer.call(id, stream);
-        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
-    });
+// Make Call By Entering ID
+$("#btnCall").on("click", () => {
+	const id = $("#remoteId").val();
+	const peerId = $("#my-peer").text();
+	if (id === peerId) return alert("Khong the thuc hien cuoc goi cho ban than");
+	openStream().then((stream) => {
+		const call = peer.call(id, stream);
+		call.on("stream", (remoteStream) =>
+			playStream("remoteStream", remoteStream)
+		);
+	});
 });
 
 // Answer Call
-peer.on('call', call => {
-    openStream()
-    .then(stream => {
-        call.answer(stream);
-        //playStream('localStream', stream);
-        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
-    });
+peer.on("call", (call) => {
+	openStream().then((stream) => {
+		call.answer(stream);
+		call.on("stream", (remoteStream) =>
+			playStream("remoteStream", remoteStream)
+		);
+	});
 });
